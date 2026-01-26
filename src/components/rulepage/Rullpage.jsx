5d010127e04f574e4b20b66f88a8f3773f95b992
jashobanta Behera <beherajashobanta892@gmail.com>
2026-01-26 23:27:13 +0530

    commite

---
+++ src/components/rulepage/Rullpage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Rullpage = () => {
+  return (
+    <section className={styles.rulesSection}>
+  <h1>How to play dice game</h1>
+
+  <p>Select any number</p>
+  <p>Click on dice image</p>
+  <p>
+    After click on dice, if selected number is equal to dice number you will get
+    same point as dice
+  </p>
+  <p>If you get wrong guess then 2 point will be deducted</p>
+</section>
+
+  )
+}
+
+export default Rullpage
